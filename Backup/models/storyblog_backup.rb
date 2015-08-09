ee358c3539ae439f476a6e5d013384a8e532398b
Ryo Kato <katoryo55@gmail.com>
2015-08-09 16:13:14 +0900

    oops

--- Backup/models/storyblog_backup.rb
+++ Backup/models/storyblog_backup.rb
@@ -93,7 +93,7 @@ Model.new(:storyblog_backup, 'Backup storyblog data to S3') do
     mail.port                 = 587
     mail.domain               = "gmail.com"
     mail.user_name            = ENV['GMAIL_USERNAME_DENKINOVEL']
-    mail.password             = ENV['GMAIL_PASSWORD_DENKINOVEL']
+    #mail.password             = ENV['GMAIL_PASSWORD_DENKINOVEL']
     mail.password             = ENV['GMAIL_APPLICATION_SPECIFIC_PASSWORD_DENKINOVEL']
     mail.authentication       = "plain"
     mail.encryption           = :starttls
